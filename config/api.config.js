35949a7f52f3dbc70facf3049eb19bbe7afd7f92
matto <101177978+bokibokigalclub@users.noreply.github.com>
2023-06-19 19:38:03 +0800

    use new account

--- config/api.config.js
+++ config/api.config.js
@@ -10,12 +10,12 @@
 module.exports = {
   // The clientId and clientSecret are used to authenticate the user with Microsoft Graph API using OAuth. You would
   // not need to change anything here if you can authenticate with your personal Microsoft account with OneDrive International.
-  clientId: 'd87bcc39-1750-4ca0-ad54-f8d0efbb2735',
-  obfuscatedClientSecret: 'U2FsdGVkX1830zo3/pFDqaBCVBb37iLw3WnBDWGF9GIB2f4apzv0roemp8Y+iIxI3Ih5ecyukqELQEGzZlYiWg==',
+  clientId: '26bca8a0-463f-44ae-baaf-eced18b93d98',
+  obfuscatedClientSecret: 'U2FsdGVkX1+ZyUSIMBbYw6+DaaqmvTK69L3509TeMGuGAmvZDw3YaOM8atw/kEbPRZ4EZo54XA3xREXhz5XDLg==',
 
   // The redirectUri is the URL that the user will be redirected to after they have authenticated with Microsoft Graph API.
   // Likewise, you would not need to change redirectUri if you are using your personal Microsoft account with OneDrive International.
-  redirectUri: 'http://localhost',
+  redirectUri: 'http://localhost:2334',
 
   // These are the URLs of the OneDrive API endpoints. You would not need to change anything here if you are using OneDrive International
   // or E5 Subscription OneDrive for Business. You may need to change these if you are using OneDrive 世纪互联.
@@ -24,10 +24,6 @@ module.exports = {
 
   // The scope we require are listed here, in most cases you would not need to change this as well.
   scope: 'user.read files.read.all offline_access',
-
-  // The directLinkRegex is used to match the direct link of the file from the response of the API. We originally use this to prevent
-  // unauthorised use of the proxied download feature - but that is disabled for now. So you can safely ignore this settings.
-  directLinkRegex: 'public[.].*[.]files[.]1drv[.]com',
 
   // Cache-Control header, check Vercel documentation for more details. The default settings imply:
   // - max-age=0: no cache for your browser
